e22048cba5717b16cd86f9f4bca9a502aa9857a4
wanglin <2842979502@qq.com>
2019-10-25 20:28:19 +0800

    基础页面搞定

---
+++ src/components/link-button/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.less'
+
+// function LinkButton(props) {
+//     return <button className='link-button' {...props}/>
+    
+// }
+// export default LinkButton
+
+function LinkButton(props) {
+    return <button className='link-button' {...props} />
+    
+}
+export default LinkButton 
--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -2,50 +2,62 @@
 后台管理主路由组件
  */
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-// import {Redirect} from 'react-router-dom'
-import {removeUserToken} from '../../redux/action-creators/user'
-import { reqUsers } from '../../api'
+
+// import {Redirect} from 'react-router-dom
+
 import WithCheckLogin from '../with-check-login'
+import { Layout } from 'antd'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import LeftNav from './left-nav'
+import AdminHeader from './header'
 
 
 
-@connect(
-  state => ({user:state.user.user}),
-  {removeUserToken}
-)
+
+import Home from '../../components/home'
+import Category from '../category'
+import Product from '../product'
+import Role from '../role'
+import User from '../user'
+import Line from '../../components/charts/line'
+import Bar from '../../components/charts/bar'
+import Pie from '../../components/charts/pie'
+
+
+const { Header, Footer, Sider, Content } = Layout
+
 @WithCheckLogin
 
 class Admin extends Component {
-  logout = () =>{
-    this.props.removeUserToken()
-  }
 
-  getUsers = async () =>{
-    reqUsers()
-    const result = await reqUsers()
-    console.log('result', result)
-  }
   render() {
-
-    // if (!this.props.hasLogin) {
-    //   // return <Redirect to="/login"/> 
-    //   return <Redirect to="/login"/>
-      
-    // }
-
-
     return (
-
-      <div>
-        <p>你好{this.props.user.username}</p>
-        <button onClick = {this.logout}>退出登陆</button>
-        <button onClick = {this.getUsers}>获取用户列表</button>
-
-
-      </div>
+      <Layout style={{height: '100%'}}>
+        <Sider>
+          <LeftNav/>
+        </Sider>
+        <Layout>
+          <AdminHeader/>
+          <Content style={{backgroundColor: 'white', margin: '30px 15px 0 15px'}}>
+            <Switch>
+              <Route path="/home" component={Home}/>
+              <Route path="/category" component={Category}/>
+              <Route path="/product" component={Product}/>
+              <Route path="/role" component={Role}/>
+              <Route path="/user" component={User}/>
+              <Route path="/charts/line" component={Line}/>
+              <Route path="/charts/bar" component={Bar}/>
+              <Route path="/charts/pie" component={Pie}/>
+              <Redirect to="/home"/>
+            </Switch>
+          </Content>
+          <Footer style={{textAlign: 'center'}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+        </Layout>
+      </Layout>
     )
   }
+  
+ 
 }
 
 
